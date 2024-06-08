f4e1ecb7b92d9e14ba6977b8c3da09fa735feafa
mohmed-011 <92048642+mohmed-011@users.noreply.github.com>
2024-06-08 14:41:53 +0200

    frist  time

--- JS/main.js
+++ JS/main.js
@@ -1,116 +1,133 @@
-var bookmarkName = document.querySelector("#bookmarkName");
-var bookmarkUrl = document.querySelector("#bookmarkUrl");
-var bookmarkSearch = document.querySelector("#bookmarkSearch");
-var submitBtn = document.querySelector("#submitBtn");
-var updateBtn = document.querySelector("#updateBtn");
-var tbody = document.querySelector("tbody");
-var deletetBt = document.querySelector("#deletetBtn");
-var visitBtn = document.querySelector("#visitBtn");
-var layer = document.querySelector(".layer");
-var closeBtn = document.querySelector(".closeBtn");
-// var cursor = document.querySelector(".cursor");
-var myIndex;
-var booklist = [];
+var signupName = document.querySelector("#signupName");
+var signupEmail = document.querySelector("#signupEmail");
+var signupPassword = document.querySelector("#signupPassword");
+var signinEmail = document.querySelector("#signinEmail");
+var signinPassword = document.querySelector("#signinPassword");
+
+var uName = document.querySelector("#uName");
+
+var incorrect = document.querySelector("#incorrect");
+var Sucssec = document.querySelector("#Sucssec");
+
+var signinBtn = document.querySelector("#signinBtn");
+var loginBtn = document.querySelector("#loginBtn");
+var logoutBtn = document.querySelector("#logoutBtn");
+
+var toSignup = document.querySelector("#toSignup");
+var toLogin = document.querySelector("#toLogin");
+
+var homePage = document.querySelector("#homePage");
+var log = document.querySelector(".log");
+var sign = document.querySelector(".sign");
+
 var regx = {
-  bookmarkName: /^[a-z]{3,10}$/,
-  bookmarkUrl: /^(ftp|http|https):\/\/[^ \"]+$/,
+  signupName: /^[a-z]{3,10}$/,
+  signupEmail: /^\w+@gmail.com$/,
+  signupPassword: /^\w+$/,
 };
 
-if (localStorage.getItem("bookmark") != null) {
-  booklist = JSON.parse(localStorage.getItem("bookmark"));
+var UserList = [];
+
+signupName.addEventListener("input", function (e) {
+  vaildation(e.target);
+});
+signupEmail.addEventListener("input", function (e) {
+  vaildation(e.target);
+});
+signupPassword.addEventListener("input", function (e) {
+  vaildation(e.target);
+});
+
+if (localStorage.getItem("USERS") != null) {
+  UserList = JSON.parse(localStorage.getItem("USERS"));
 }
-dispaly();
 
-// ===== Events ==========
-submitBtn.addEventListener("click", function () {
-  addbookmark();
-  dispaly();
-  clear();
-});
-bookmarkName.addEventListener("input", function (e) {
-  vaildation(e.target);
-});
-bookmarkUrl.addEventListener("input", function (e) {
-  vaildation(e.target);
-});
-closeBtn.addEventListener("click", function () {
-  layer.classList.replace("d-flex", "d-none");
-});
-updateBtn.addEventListener("click", function () {
-  Update();
-  dispaly();
-});
-bookmarkSearch.addEventListener("input", function (e) {
-  var word = e.target.value;
-  var cartona = ``;
-  for (var i = 0; i < booklist.length; i++) {
-    if (booklist[i].webName.toLowerCase().includes(word.toLowerCase())) {
-      cartona += `
-      <tr>
-            <td class="text-center py-3" >${i + 1}</td>
-            <td class="text-center py-3" >${booklist[i].webName}</td>
-            <td class="text-center py-3"><a id="visitBtn" target="_blank" class="btn btn-outline-success rounded-pill" href="${
-              booklist[i].webUrl
-            }"> <i class="fa-solid fa-eye pe-2 fa-beat-fade "></i> Visit</a></td>
-            <td class="text-center py-3"><button id="deletetBtn" onclick="Set_up(${i})" class="btn btn-outline-warning rounded-pill">  <i class="fa-solid fa-pencil pe-2 "></i></i> Update</button></td>
-            <td class="text-center py-3"><button id="deletetBtn" onclick="Delete(${i})" class="btn btn-outline-danger rounded-pill">  <i class="fa-solid fa-trash-can pe-2 fa-shake"></i> Delete</button></td>
-      </tr>
-      `;
-    }
-    tbody.innerHTML = cartona;
-  }
+signinBtn.addEventListener("click", function () {
+  addUser();
 });
 
-// document.addEventListener("mousemove",function(e){
-//   cursor.style.left = e.clientX  + "px";
-//   cursor.style.top = e.clientY + "px";
-// })
+loginBtn.addEventListener("click", function () {
+  login();
+});
 
-// ===== Functions =======
-function addbookmark() {
+toSignup.addEventListener("click", function () {
+  sign.classList.replace("d-none", "d-block");
+  log.classList.add("d-none");
+});
+
+toLogin.addEventListener("click", function () {
+  log.classList.replace("d-none", "d-block");
+  sign.classList.replace("d-block", "d-none");
+});
+
+logoutBtn.addEventListener("click",function(){
+  homePage.classList.replace("d-block", "d-none");
+  log.classList.replace("d-none", "d-block");
+})
+
+function addUser() {
+  var found = false;
   if (
-    bookmarkUrl.classList.contains("is-valid") &&
-    bookmarkName.classList.contains("is-valid")
+    signupName.value == "" ||
+    signupEmail.value == "" ||
+    signupPassword.value == ""
   ) {
-    var website = {
-      webName: bookmarkName.value,
-      webUrl: bookmarkUrl.value,
-    };
-    booklist.push(website);
-    localStorage.setItem("bookmark", JSON.stringify(booklist));
-
-    bookmarkUrl.classList.remove("is-valid");
-    bookmarkName.classList.remove("is-valid");
+    Sucssec.innerHTML = "All inputs is required";
   } else {
-    layer.classList.replace("d-none", "d-flex");
+    if (
+      signupName.classList.contains("is-valid") &&
+      signupEmail.classList.contains("is-valid") &&
+      signupPassword.classList.contains("is-valid")
+    ) {
+      for (var i = 0; i < UserList.length; i++) {
+        if (signupEmail.value == UserList[i].userEmail) {
+          Sucssec.innerHTML = "This Email is alrady used";
+          found = true;
+        }
+      }
+      if (!found) {
+        var Userinfo = {
+          userName: signupName.value,
+          userEmail: signupEmail.value,
+          userBassword: signupPassword.value,
+        };
+        UserList.push(Userinfo);
+        localStorage.setItem("USERS", JSON.stringify(UserList));
+        log.classList.replace("d-none", "d-block");
+        sign.classList.replace("d-block", "d-none");
+      }
+    } else {
+      Sucssec.innerHTML = "Not valid data";
+    }
   }
 }
-function dispaly() {
-  var cartona = "";
-  for (var i = 0; i < booklist.length; i++) {
-    cartona += `
-      <tr>
-            <td class="text-center py-3" >${i + 1}</td>
-            <td class="text-center py-3" >${booklist[i].webName}</td>
-            <td class="text-center py-3"><a id="visitBtn" target="_blank" class="btn btn-outline-success rounded-pill" href="${
-              booklist[i].webUrl
-            }"> <i class="fa-solid fa-eye pe-2 fa-beat-fade "></i> Visit</a></td>
-            <td class="text-center py-3"><button id="deletetBtn" onclick="Set_up(${i})" class="btn btn-outline-warning rounded-pill">  <i class="fa-solid fa-pencil pe-2 fa-fade"></i></i> Update</button></td>
-            <td class="text-center py-3"><button id="deletetBtn" onclick="Delete(${i})" class="btn btn-outline-danger rounded-pill">  <i class="fa-solid fa-trash-can pe-2 fa-shake"></i> Delete</button></td>
-      </tr>
-      `;
+
+function login() {
+  var email = signinEmail.value;
+  var Password = signinPassword.value;
+  var found = false;
+
+  if (email == "" || Password == "") {
+    incorrect.innerHTML = "All inputs is required";
+  } else {
+    for (var i = 0; i < UserList.length; i++) {
+      if (
+        email == UserList[i].userEmail &&
+        Password == UserList[i].userBassword
+      ) {
+        uName.innerHTML = UserList[i].userName;
+        homePage.classList.replace("d-none", "d-block");
+        log.classList.add("d-none");
+        found = true;
+      }
+    }
+
+    if (found == false) {
+      incorrect.innerHTML = "incorrect  Email  or  Password";
+    }
   }
-  tbody.innerHTML = cartona;
 }
-function Delete(index) {
-  booklist.splice(index, 1);
-  localStorage.setItem("bookmark", JSON.stringify(booklist));
-  dispaly();
-}
-function clear() {
-  bookmarkName.value = "";
-  bookmarkUrl.value = "";
-}
+
 function vaildation(element) {
   if (regx[element.id].test(element.value)) {
     element.classList.add("is-valid");
@@ -118,36 +135,5 @@ function vaildation(element) {
   } else {
     element.classList.remove("is-valid");
     element.classList.add("is-invalid");
-  }
-}
-
-function Set_up(index) {
-  myIndex = index;
-
-  bookmarkName.value = booklist[index].webName;
-  bookmarkUrl.value = booklist[index].webUrl;
-
-  submitBtn.classList.add("d-none");
-  updateBtn.classList.replace("d-none", "d-block");
-
-  bookmarkUrl.classList.add("is-valid");
-  bookmarkName.classList.add("is-valid");
-}
-function Update() {
-  if (
-    bookmarkUrl.classList.contains("is-valid") &&
-    bookmarkName.classList.contains("is-valid")
-  ) {
-    booklist[myIndex].webName = bookmarkName.value;
-    booklist[myIndex].webUrl = bookmarkUrl.value;
-
-    localStorage.setItem("bookmark", JSON.stringify(booklist));
-    updateBtn.classList.replace("d-block", "d-none");
-    submitBtn.classList.replace("d-none", "d-block");
-    bookmarkUrl.classList.remove("is-valid");
-    bookmarkName.classList.remove("is-valid");
-    clear();
-  } else {
-    layer.classList.replace("d-none", "d-flex");
   }
 }
